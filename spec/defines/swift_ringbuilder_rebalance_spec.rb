a212d0b98d3bb1336fd8ea65b063f62f05c2bb87
Ivan Berezovskiy <iberezovskiy@mirantis.com>
2015-10-06 09:28:40 +0000

    Revert "Make ring rebalance more reliable."
    This reverts commit 02ed7487ca6a83671ac5caca16987c3b07aa094a.
    
    Change-Id: Iad3d1a4f409509aa88effc407bdeab5cf3ddeacc

--- spec/defines/swift_ringbuilder_rebalance_spec.rb
+++ spec/defines/swift_ringbuilder_rebalance_spec.rb
@@ -9,14 +9,8 @@ describe 'swift::ringbuilder::rebalance' do
         it { is_expected.to contain_exec("rebalance_#{type}").with(
           {:command     => "swift-ring-builder /etc/swift/#{type}.builder rebalance",
            :path        => ['/usr/bin'],
-           :refreshonly => true,
-           :returns     => [0,1]}
-        )}
-        it { is_expected.to contain_exec("hours_passed_#{type}").with(
-          {:command     => "swift-ring-builder /etc/swift/#{type}.builder pretend_min_part_hours_passed",
-           :path        => ['/usr/bin'],
            :refreshonly => true}
-        ).that_comes_before("Exec[rebalance_#{type}]")}
+        )}
       end
     end
   end
@@ -30,14 +24,8 @@ describe 'swift::ringbuilder::rebalance' do
     it { is_expected.to contain_exec("rebalance_object").with(
       {:command     => "swift-ring-builder /etc/swift/object.builder rebalance 999",
        :path        => ['/usr/bin'],
-       :refreshonly => true,
-       :returns     => [0,1]}
-    )}
-    it { is_expected.to contain_exec("hours_passed_object").with(
-      {:command     => "swift-ring-builder /etc/swift/object.builder pretend_min_part_hours_passed",
-       :path        => ['/usr/bin'],
        :refreshonly => true}
-    ).that_comes_before("Exec[rebalance_object]")}
+    )}
   end
   describe 'with an invalid seed' do
     let :title do
