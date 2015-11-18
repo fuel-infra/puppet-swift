a1a229a72b125ff1996712cfe7094e3fd6827c7a
Denis Egorenko <degorenko@mirantis.com>
2015-11-18 18:30:35 +0300

    Update ceilometer proxy template
    Now for using Ceilometer notifications from Swift we should
    use Ceilometer middleware. Hence we need update template for Swift
    to account for incoming and outgoing traffic notifications.
    
    [1] I686e5d94fb72fd2a00973c91673edc417142ee2f
    [2] http://docs.openstack.org/developer/oslo.messaging/transport.html#oslo_messaging.TransportURL
    
    Change-Id: I524a285fad64be452dfda392a7e68a5c35b54f7d
    Related-bug: #1510064

--- manifests/proxy/ceilometer.pp
+++ manifests/proxy/ceilometer.pp
@@ -15,6 +15,11 @@
 #   (Optional) Port of the rabbit server.
 #   Defaults to 5672.
 #
+# [*rabbit_hosts*]
+#   (Optional) IP or hostname of the rabbits servers.
+#   comma separated array (ex: ['1.0.0.10:5672','1.0.0.11:5672'])
+#   Defaults to undef.
+#
 # [*rabbit_user*]
 #   (Optional) Username for rabbit.
 #   Defaults to 'guest'.
@@ -22,6 +27,22 @@
 # [*rabbit_password*]
 #   (Optional) Password for rabbit user.
 #   Defaults to 'guest'.
+#
+# [*rabbit_virtual_host*]
+#   (Optional) Virtual host to use.
+#   Defaults to '/'.
+#
+# [*driver*]
+#   (Optional) The Drivers(s) to handle sending notifications.
+#   Defaults to undef.
+#
+# [*topic*]
+#   (Optional) AMQP topic used for OpenStack notifications.
+#   Defaults to undef.
+#
+# [*control_exchange*]
+#   (Optional) The default exchange under which topics are scoped.
+#   Defaults to undef.
 #
 # [*ensure*]
 #   Enable or not ceilometer fragment
@@ -44,12 +65,17 @@
 # Copyright 2013 eNovance licensing@enovance.com
 #
 class swift::proxy::ceilometer(
-  $rabbit_user     = 'guest',
-  $rabbit_password = 'guest',
-  $rabbit_host     = '127.0.0.1',
-  $rabbit_port     = '5672',
-  $ensure          = 'present',
-  $group           = 'ceilometer',
+  $rabbit_user         = 'guest',
+  $rabbit_password     = 'guest',
+  $rabbit_host         = '127.0.0.1',
+  $rabbit_port         = '5672',
+  $rabbit_hosts        = undef,
+  $rabbit_virtual_host = '/',
+  $driver              = undef,
+  $topic               = undef,
+  $control_exchange    = undef,
+  $ensure              = 'present',
+  $group               = 'ceilometer',
 ) inherits swift {
 
   User['swift'] {
